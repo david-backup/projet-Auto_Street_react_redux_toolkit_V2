3a4462adb8f3aa946c8072c0dd0123f35d9878c0
David Changea <14ir155@gmail.com>
2024-03-18 11:49:03 +0100

    initialisation des routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ContactPage from "./pages/ContactPage";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="contact" element={<ContactPage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
